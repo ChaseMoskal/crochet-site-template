f0c970c2560210389b782bfdcd1e9728e8262a3b
Chase Moskal <lordbrimshaw@gmail.com>
2017-10-22 19:39:42 -0700

    convert to tabs

--- s/crochet-script.ts
+++ s/crochet-script.ts
@@ -4,23 +4,23 @@ import {readFile, readFiles, writeFile, writeFiles} from "crochet/o/files"
 
 async function main() {
 
-  // Render pages
-  await writeFiles(await new Crochet({
-    srcdir: "s/pages",
-    outdir: "o"
-  }).pages({
-    pages: await readFiles("s/pages/*.md"),
-    template: await readFile("s/templates/page.crochet.html")
-  }))
+	// Render pages
+	await writeFiles(await new Crochet({
+		srcdir: "s/pages",
+		outdir: "o"
+	}).pages({
+		pages: await readFiles("s/pages/*.md"),
+		template: await readFile("s/templates/page.crochet.html")
+	}))
 
-  // Render blog posts
-  await writeFiles(await new Crochet({
-    srcdir: "s",
-    outdir: "o"
-  }).pages({
-    pages: await readFiles("s/blog/*.md"),
-    template: await readFile("s/templates/blog-post.crochet.html")
-  }))
+	// Render blog posts
+	await writeFiles(await new Crochet({
+		srcdir: "s",
+		outdir: "o/blog"
+	}).pages({
+		pages: await readFiles("s/blog/*.md"),
+		template: await readFile("s/templates/blog-post.crochet.html")
+	}))
 
 }
 
