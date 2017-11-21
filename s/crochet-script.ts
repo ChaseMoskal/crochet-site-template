550e46b111682177b958bff192a1082699d2dbc7
Chase Moskal <lordbrimshaw@gmail.com>
2017-11-20 16:55:39 -0800

    update crochet package, update license

--- s/crochet-script.ts
+++ s/crochet-script.ts
@@ -2,26 +2,30 @@
 import Crochet from "crochet"
 import {readFile, readFiles, writeFile, writeFiles} from "crochet/o/files"
 
-async function main() {
+(async() => {
 
 	// Render pages
-	await writeFiles(await new Crochet({
-		srcdir: "s/pages",
-		outdir: "o"
-	}).pages({
-		pages: await readFiles("s/pages/*.md"),
-		template: await readFile("s/templates/page.crochet.html")
-	}))
+	await writeFiles(
+		await new Crochet({
+			srcdir: "s/pages",
+			outdir: "o"
+		}).pages({
+			pages: await readFiles("s/pages/*.md"),
+			template: await readFile("s/templates/page.crochet.html"),
+			context: {}
+		})
+	)
 
 	// Render blog posts
-	await writeFiles(await new Crochet({
-		srcdir: "s",
-		outdir: "o"
-	}).pages({
-		pages: await readFiles("s/blog/*.md"),
-		template: await readFile("s/templates/blog-post.crochet.html")
-	}))
+	await writeFiles(
+		await new Crochet({
+			srcdir: "s",
+			outdir: "o"
+		}).pages({
+			pages: await readFiles("s/blog/*.md"),
+			template: await readFile("s/templates/blog-post.crochet.html"),
+			context: {}
+		})
+	)
 
-}
-
-main()
+})()
